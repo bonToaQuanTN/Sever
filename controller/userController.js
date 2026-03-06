b938903423707d99344f753fb57ed6eaeb4da9c8
bonToaQuanTN <quantctin@gmail.com>
2026-03-06 16:49:55 +0700

    clean n+1

--- controller/userController.js
+++ controller/userController.js
@@ -6,42 +6,27 @@ export const getAll = async (req, res) => {
     const users = await UserModel.findAll();
 
     if (users.length === 0) {
-      return res.status(404).json({
-        message: "No employees found"
-      });
+      return res.status(404).json({message: "No employees found"});
     }
     return res.json(users);
   } catch (error) {
-
     console.error(error);
-    res.status(500).json({
-      message: "Internal server error"
-    });
+    res.status(500).json({message: "Internal server error"});
   }
 };
 
 export const getId = async (req, res) => {
   const empid = req.params.empid;
   try {
-
-    const emp = await UserModel.findOne({
-      where: { empid }
-    });
+    const emp = await UserModel.findOne({where: { empid }});
 
     if (!emp) {
-      return res.status(404).json({
-        message: "Employee not found"
-      });
+      return res.status(404).json({message: "Employee not found"});
     }
-
     res.json(emp);
 
   } catch (error) {
-
-    res.status(500).json({
-      message: "Internal server error"
-    });
-
+    res.status(500).json({message: "Internal server error"});
   }
 };
 
@@ -58,34 +43,20 @@ export const postEmp = async (req, res) => {
   const { name, email, designation, empid } = req.body;
 
   try {
-
     const exist = await UserModel.findOne({
       where: { empid }
     });
 
     if (exist) {
-      return res.status(409).json({
-        message: "Employee already exists"
-      });
+      return res.status(409).json({message: "Employee already exists"});
     }
 
-    const employee = await UserModel.create({
-      name,
-      email,
-      designation,
-      empid
-    });
-
+    const employee = await UserModel.create({name,email, designation, empid});
     res.status(201).json(employee);
 
   } catch (error) {
-
     console.error(error);
-
-    res.status(500).json({
-      message: "Internal server error"
-    });
-
+    res.status(500).json({message: "Internal server error"});
   }
 };
 
@@ -93,43 +64,30 @@ export const putEmp = async (req, res) => {
   const empid = req.params.empid;
 
   try {
-    const [updated] = await UserModel.update(
-      req.body,{ where: { empid } 
-    });
-
+    const [updated] = await UserModel.update(req.body,{ where: { empid }});
     if (updated === 0) {
-      return res.status(404).json({
-        message: "Employee not found"
-      });
+      return res.status(404).json({message: "Employee not found"});
     }
-
-    return res.json({
-      message: "Update successfully"
-    });
+    return res.json({message: "Update successfully"});
 
   } catch (error) {
     console.error(error);
-    res.status(500).json({
-        message: "Internal server error"
-    });
+    res.status(500).json({message: "Internal server error"});
   }
 };
 
 export const deleteEmp = async (req, res) => {
   const empid = req.params.empid;
   try {
-    const emp = await UserModel.findOne({
-      where: { empid }
-    });
+    const emp = await UserModel.findOne({where: { empid }});
+
     if (!emp) {
-      return res.status(404).json({
-        message: "Employee not found"
-      });
+      return res.status(404).json({message: "Employee not found"});
     }
     await emp.destroy();
-    res.json({
-      message: "Deleted successfully"
-    });
+
+    res.json({message: "Deleted successfully"});
+    
   } catch (error) {
     res.status(500).json({ message: "Internal server error"});
   }
