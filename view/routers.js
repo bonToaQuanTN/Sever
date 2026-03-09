2dfc574b979d09931f477d01f01a9bebb36fad0e
bonToaQuanTN <quantctin@gmail.com>
2026-03-09 15:24:27 +0700

    check token

--- view/routers.js
+++ view/routers.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { getAll, postEmp, getId, putEmp, deleteEmp} from "../controller/userController.js";
 import { employeeValidation } from "../validators/employeeValidator.js";
+import { delay } from "../Middleware/delay.js"
 
 const router = express.Router();
+
+router.use(delay);
 
 router.get("/employees", getAll);
 router.get("/employees/:empid", getId);
