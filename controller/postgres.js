f01437ed87f4c0312b412ed1ba2f8378383c457c
bonToaQuanTN <quantctin@gmail.com>
2026-03-17 09:38:41 +0700

    update

--- controller/postgres.js
+++ controller/postgres.js
@@ -1,59 +1,22 @@
-import { Sequelize } from 'sequelize';
-import { createUserModel } from '../model/userData.js';
-import bcrypt from "bcrypt";
+import { Module } from "@nestjs/common";
+import { SequelizeModule } from "@nestjs/sequelize";
+import { EmployeeModule } from "../app.module";
+import { Employee } from "../app.model";
 
-const sequelize = new Sequelize('crud_db', 'postgres', 'MinWan', {
-  host: 'localhost',
-  port: 6969,
-  dialect: 'postgres'
-});
-
-let UserModel = null;
-
-const connection = async () => {
-  try {
-
-    await sequelize.authenticate();
-    console.log("Database connected successfully");
-
-    // create model
-    UserModel = await createUserModel(sequelize);
-
-    // sync database
-    await sequelize.sync({ alter: true });
-    console.log("Database synced");
-
-    // create admin
-    await createAdmin();
-
-  } catch (error){
-    console.error("Database connection failed:", error);
-  }
-};
-
-const createAdmin = async () => {
-  const admin = await UserModel.findOne({
-    where:{ email:"admin@gmail.com" }
-  });
-
-  if(!admin){
-    const hash = await bcrypt.hash("toibjngu",10);
-    await UserModel.create({
-      name:"Admin",
-      email:"admin@gmail.com",
-      password:hash,
-      designation:"Administrator",
-      empid:"ADMIN001",
-      role:"admin"
-    });
-
-    console.log("Admin account created");
-  }
-
-};
-
-export {
-  connection,
-  sequelize,
-  UserModel
-};
+@Module({
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: "postgres",
+      host: "localhost",
+      port: 6969,
+      username: "postgres",
+      password: "MinWan",
+      database: "crud_db",
+      autoLoadModels: true,
+      synchronize: true,
+      models: [Employee]
+    }),
+    EmployeeModule
+  ],
+})
+export class AppModule {}
